978b655c293f95611a5d93a26a659d9ce1984004
Jose Terrero <josetg999@gmail.com>
2019-01-10 10:01:49 +0100

    nuevo reparto y main

--- Monopoly/src/test/Ppal.java
+++ Monopoly/src/test/Ppal.java
@@ -1,5 +1,6 @@
 package test;
 
+import controller.DadoController;
 import datos.JugadorDatos;
 import lecturaporteclado.Leer;
 import model.Jugador;
@@ -15,36 +16,43 @@ public class Ppal {
 		double dinero = 10000;
 		// Tablero tab = new Tablero();
 		JugadorDatos jugDat = new JugadorDatos();
-	//	Tablero t = new Tablero();
+		Tablero t = new Tablero();
+		DadoController dadoCont = new DadoController();
 
 		Jugador[] listaJugadores = new Jugador[tam];
 
 		// tab.imprimirNombreJuego();
 
 		System.out.println("Bienvenidos al Monopoly.");
-		
+
 		jugDat.arrayInstrucciones();
 
 		System.out.println("Vamos a crear a los jugadores. ");
 
 		for (int i = 0; i < listaJugadores.length; i++) {
-			System.out.println("Introduce el nombre del jugador "+(i+1)+": ");
+			System.out.println("Introduce el nombre del jugador " + (i + 1) + ": ");
 			nombre = Leer.dato();
 
-			listaJugadores[i] = new Jugador(nombre, dinero, posicion, player, posAnterior); //Creo que si los datos se los vamos a pasar luego de çbemos crear los jugadores vacios y luego se rellenan
+			listaJugadores[i] = new Jugador(nombre, dinero, posicion, player, posAnterior); // Creo que si los datos se
+																							// los vamos a pasar luego
+																							// de çbemos crear los
+																							// jugadores vacios y luego
+																							// se rellenan
 			player++;
 		}
-		
 
-/*
-		for (int i = 0; i < listaJugadores.length; i++) {
-			System.out.println("Jugador " + (i + 1) + " - " + listaJugadores[i]);
-		}
-*/
+		/*
+		 * for (int i = 0; i < listaJugadores.length; i++) {
+		 * System.out.println("Jugador " + (i + 1) + " - " + listaJugadores[i]); }
+		 */
 		
-		
-		
-		
+		// t.imprimirTablero(tablero);
+
+		t.getTablero();
+
+		System.out.println("Va a empezar el Jugador 1.");
+
+		System.out.println("Ha salido un " + dadoCont.tirarDado() + " del dado.");
 
 	}
 
